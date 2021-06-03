cc38cf9376e58dd46b0736de67b78c56108d1c00
heeyoung1002 <78261017+heeyoung1002@users.noreply.github.com>
2021-06-03 23:29:37 +0900

    DAY9-Solving BaekJoon Questions

---
+++ DAY9-2.C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void){
+    
+    int n;
+    scanf("%d", &n);
+    
+    if(n>=90 && n<=100){
+        printf("A");
+    }
+    else if(n>=80 && n<=89){
+        printf("B");
+    }
+    else if(n>=70 && n<=79){
+        printf("C");
+    }
+    else if(n>=60 && n<=69){
+        printf("D");
+    }
+    else{
+        printf("F");
+    }
+    
+    return 0;
+}
